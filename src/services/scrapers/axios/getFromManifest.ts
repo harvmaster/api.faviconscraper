5f5116b8848e7ec7c8b0813fb91ffb223d053ab0
Harvmaster <hzuccon@gmail.com>
2024-05-18 19:29:53 +1000

    Removed commented code from getFromManifest

--- src/services/scrapers/axios/getFromManifest.ts
+++ src/services/scrapers/axios/getFromManifest.ts
@@ -1,22 +1,27 @@
-import axios from 'axios';
-import { getResponseDomain } from './utils'
+import axios, { AxiosResponse } from 'axios';
+import { load } from 'cheerio'
 import { RawIcon } from '../types';
 
-export const getFromManifest = async (url: string): Promise<RawIcon[]> => {
-  const response = await axios.get(`https://${url}/manifest.json`)
-  const { data } = response
+export const getFromManifest = async (response: AxiosResponse): Promise<RawIcon[]> => {
+  const data = response.data
 
-  const domain: string = getResponseDomain(response)
-  if (domain.endsWith('/')) domain.slice(0, -1)
+  const $ = load(data)
 
-  const icons: string[] = data.icons.map((icon: { src: string }) => icon.src)
-  .map((icon: string) => {
-    if (icon.startsWith('http')) return icon
-    if (icon.startsWith('/')) return `${domain}${icon}`
-    return `${domain}/${icon}`
-  })
+  const manifestArr = $('link[rel="manifest"]').map((i, element) => {
+    const href = $(element).attr('href')
+    return href
+  }).get()
 
-  return icons.map(icon => ({ src: icon, source: 'mobile' }))
+  if (manifestArr.length === 0) return []
+
+  const manifest = manifestArr[0]
+
+  const manifestResponse = await axios.get(manifest)
+  const manifestData = manifestResponse.data
+
+  const icons = manifestData.icons.map((icon: { src: string }) => icon.src)
+
+  return icons.map(icon => ({ src: icon, source: 'manifest' }))
 }
 
 export default getFromManifest
