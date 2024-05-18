f9263f6ec4b8dad00c2253e62d370ec363bbd3e2
Harvmaster <hzuccon@gmail.com>
2024-05-18 19:30:39 +1000

    Cleaned up code for getFromManifest

--- src/services/scrapers/axios/getFromManifest.ts
+++ src/services/scrapers/axios/getFromManifest.ts
@@ -3,19 +3,15 @@ import { load } from 'cheerio'
 import { RawIcon } from '../types';
 
 export const getFromManifest = async (response: AxiosResponse): Promise<RawIcon[]> => {
-  const data = response.data
-
-  const $ = load(data)
+  const $ = load(response.data)
 
   const manifestArr = $('link[rel="manifest"]').map((i, element) => {
     const href = $(element).attr('href')
     return href
   }).get()
-
   if (manifestArr.length === 0) return []
 
   const manifest = manifestArr[0]
-
   const manifestResponse = await axios.get(manifest)
   const manifestData = manifestResponse.data
 
